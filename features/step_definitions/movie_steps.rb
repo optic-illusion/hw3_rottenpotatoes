4d3a3de5b3927c51fa557ef76a52b9011dbf90ab
Ubuntu <ubuntu@ip-10-195-175-12.ec2.internal>
2012-08-08 14:48:31 +0000

    Finished 2a

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -2,10 +2,11 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
+    Movie.find_or_create_by_title_and_rating(:title => movie[:title], :rating => movie[:rating], :release_date => movie[:release_date])
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
   end
-  flunk "Unimplemented"
+  #flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -22,6 +23,13 @@ end
 #  "When I check the following ratings: G"
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  rating_list.split.each do |rating|
+    if !uncheck then
+      step %{I check "ratings_#{rating}"}
+    else
+      step %{I uncheck "ratings_#{rating}"}
+    end
+  end
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
